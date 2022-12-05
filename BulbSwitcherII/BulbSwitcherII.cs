1a53bbc8220e69605baee4d6b3a03d8b0f6e62a9
SanjayKumar Sharma <37655226+Sanjaykumar1806@users.noreply.github.com>
2022-12-05 14:03:33 +0100

    Add files via upload

---
+++ BulbSwitcherII/BulbSwitcherII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BulbSwitcherII
+{
+    class BulbSwitcherII
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] {3, 2, 1, 5, 4 };
+            Console.WriteLine(CountOfShine(arr));
+            Console.Read();
+        }
+        public static int CountOfShine(int[] a)
+        {
+            var rightMax = -1;
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                rightMax = Math.Max(rightMax, a[i]);
+                if (i + 1 == rightMax)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }   
+}
